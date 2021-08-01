9d64482fc694e2d9d1baa62efdab375230326a46
Hoa Nguyen <hoanguyen@gmail.com>
2021-08-01 08:32:51 +0700

    Update content day 1

--- jenkins-slave/scripts/install_jenkins_slave.sh
+++ jenkins-slave/scripts/install_jenkins_slave.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
 #Create jenkins home dir at host
-mkdir -p /var/jenkins_home/.ssh
-cp ./id_rsa.pub /var/jenkins_home/.ssh/authorized_keys
+mkdir -p /var/jenkins_home_slave/.ssh
+cp ./id_rsa.pub /var/jenkins_home_slave/.ssh/authorized_keys
 
 #Change Mode for .ssh dir
-chmod 700 /var/jenkins_home/.ssh
-chmod 600 /var/jenkins_home/.ssh/authorized_keys
-chown -R 1000:1000 /var/jenkins_home
-
-#Remove RSCF
-#sed -i 's/\r$//' *
+chmod 700 /var/jenkins_home_slave/.ssh
+chmod 600 /var/jenkins_home_slave/.ssh/authorized_keys
+chown -R 1000:1000 /var/jenkins_home_slave
 
 #docker run Jenkins slave image
-docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d hoabka/jenkins-slave:latest
+docker run -p 2222:22 -v /var/jenkins_home_slave:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d --name jenkins-slave hoabka/jenkins-slave:latest
